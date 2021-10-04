a529f60375cb52510e1d1c8971595303b0ac343a
Patrik <kolesarp1@gmail.com>
2021-10-05 01:11:56 +0200

    adding init_setup script, cardano-node.service systemd service
    Signed-off-by: Patrik <kolesarp1@gmail.com>

--- init_setup.sh
+++ init_setup.sh
@@ -6,4 +6,11 @@ echo export NODE_HOME=$HOME/Desktop/cardano-relay >> $HOME/.bashrc
 #echo export NODE_CONFIG=mainnet>> $HOME/.bashrc
 echo export NODE_CONFIG=testnet>> $HOME/.bashrc
 source $HOME/.bashrc
+sudo mv $NODE_HOME/cardano-node.service /etc/systemd/system/cardano-node.service
+sudo chmod 644 /etc/systemd/system/cardano-node.service
+
+#Run the following to enable auto-starting of your stake pool at boot time.
+sudo systemctl daemon-reload
+sudo systemctl enable cardano-node
+
 echo "Cardano variables in $HOME/.bashrc set, congratulations! :-)"
---
+++ startRelayNode1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIRECTORY=$NODE_HOME
+PORT=6000
+HOSTADDR=0.0.0.0
+TOPOLOGY=\${DIRECTORY}/${NODE_CONFIG}-topology.json
+DB_PATH=\${DIRECTORY}/db
+SOCKET_PATH=\${DIRECTORY}/db/socket
+CONFIG=\${DIRECTORY}/${NODE_CONFIG}-config.json
+/usr/local/bin/cardano-node run +RTS -N -A16m -qg -qb -RTS --topology \${TOPOLOGY} --database-path \${DB_PATH} --socket-path \${SOCKET_PATH} --host-addr \${HOSTADDR} --port \${PORT} --config \${CONFIG}
