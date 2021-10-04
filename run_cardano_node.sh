91dfbaa5d3309cda86783b3b30048e548d813179
Patrik <kolesarp1@gmail.com>
2021-10-04 22:34:38 +0200

    moving configuration folder outside of node binaries version
    Signed-off-by: Patrik <kolesarp1@gmail.com>

--- run_cardano_node.sh
+++ run_cardano_node.sh
@@ -2,9 +2,9 @@
 mkdir -p /home/patrik/Desktop/cardano-relay/db/
 
 cardano-node run \
-     --topology /home/patrik/Desktop/cardano-relay/cardano-node-1.30.0-linux/configuration/cardano/testnet/testnet-topology.json \
-     --database-path /home/patrik/Desktop/cardano-relay/db \
-     --socket-path /home/patrik/Desktop/cardano-relay/db/node.socket \
+     --topology $NODE_HOME/configuration/testnet/testnet-topology.json \
+     --database-path $NODE_HOME/db \
+     --socket-path $NODE_HOME/db/node.socket \
      --host-addr 127.0.0.1 \
      --port 8081 \
-     --config /home/patrik/Desktop/cardano-relay/cardano-node-1.30.0-linux/configuration/cardano/testnet/testnet-config.json
+     --config $NODE_HOME/configuration/testnet/testnet-config.json
