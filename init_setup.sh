0624aabac8c638aab29c1efdfb593897002afbf5
Patrik <kolesarp1@gmail.com>
2021-10-04 22:32:29 +0200

    added initialization script
    Signed-off-by: Patrik <kolesarp1@gmail.com>

---
+++ init_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo export CARDANO_NODE_SOCKET_PATH="/home/patrik/Desktop/cardano-relay/db/node.socket"
+echo PATH="$HOME/.local/bin:$PATH" >> $HOME/.bashrc
+#echo export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" >> $HOME/.bashrc
+echo export NODE_HOME=$HOME/Desktop/cardano-relay >> $HOME/.bashrc
+#echo export NODE_CONFIG=mainnet>> $HOME/.bashrc
+echo export NODE_CONFIG=testnet>> $HOME/.bashrc
+source $HOME/.bashrc
+echo "Cardano variables in $HOME/.bashrc set, congratulations! :-)"
