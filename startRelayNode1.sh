c61f912b48ead5c4ad9d72c3b51ab64c9a001a27
Patrik <kolesarp1@gmail.com>
2021-10-06 09:35:37 +0200

    Introducing .gitignore ignore generated file
    Signed-off-by: Patrik <kolesarp1@gmail.com>

--- startRelayNode1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIRECTORY=/home/patrik/Desktop/cardano-relay
-PORT=6000
-HOSTADDR=0.0.0.0
-TOPOLOGY=/home/patrik/Desktop/cardano-relay/configuration/testnet-topology.json
-DB_PATH=/home/patrik/Desktop/cardano-relay/db
-SOCKET_PATH=/home/patrik/Desktop/cardano-relay/db/socket
-CONFIG=/home/patrik/Desktop/cardano-relay/configuration/testnet-config.json
-/home/patrik/Desktop/cardano-relay/bin/cardano-node run +RTS -N -A16m -qg -qb -RTS --topology ${TOPOLOGY} --database-path ${DB_PATH} --socket-path ${SOCKET_PATH} --host-addr ${HOSTADDR} --port ${PORT} --config ${CONFIG}
